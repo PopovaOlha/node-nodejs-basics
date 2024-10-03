8d7e2e044289437e8b9c990b762ad1d2e3619283
PopovaOlha <fc.odess.10@gmail.com>
2024-10-03 08:33:14 +0300

    read and print content of fileToRead.txt with error handling

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
-const read = async () => {
-    // Write your code here 
-};
+import { constants, promises as fs } from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-await read();
+const read = async () => {
+	const currentDir = path.dirname(fileURLToPath(import.meta.url))
+
+	const filePathToRead = path.join(currentDir, 'files', 'fileToRead.txt')
+
+	try {
+		await fs.access(filePathToRead, constants.F_OK)
+
+		const fileContent = await fs.readFile(filePathToRead, 'utf-8')
+
+		console.log('Content of fileToRead.txt:', fileContent)
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			console.error('FS operation failed: File "fileToRead.txt" does not exist')
+			return
+		}
+
+		console.error('FS operation failed:', error.message)
+	}
+}
+
+await read()
