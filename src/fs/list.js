fb629b7b494e6db269315cb25a218a258c3b30e4
PopovaOlha <fc.odess.10@gmail.com>
2024-10-03 08:32:17 +0300

    list all filenames from files folder with error handling

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,34 @@
-const list = async () => {
-    // Write your code here 
-};
+import { constants, promises as fs } from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await list();
+const list = async () => {
+	const currentDir = dirname(fileURLToPath(import.meta.url))
+
+	const filesFolderPath = join(currentDir, 'files')
+
+	const textFileNames = []
+
+	try {
+		await fs.access(filesFolderPath, constants.F_OK)
+
+		const fileNames = await fs.readdir(filesFolderPath)
+
+		fileNames.forEach(fileName => {
+			if (fileName.toLowerCase().endsWith('.txt')) {
+				textFileNames.push(fileName)
+			}
+		})
+
+		console.log('Array of text files in "files" folder:', textFileNames)
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			console.error('FS operation failed: Folder "files" does not exist')
+			return
+		}
+
+		console.error('FS operation failed:', error.message)
+	}
+}
+
+await list()
