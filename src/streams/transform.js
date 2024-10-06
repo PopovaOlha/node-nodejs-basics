8c06fa01ea1af90aad60ebed6d53820c20c71119
PopovaOlha <fc.odess.10@gmail.com>
2024-10-06 08:48:50 +0300

    added transform stream to reverse stdin text and output to stdout

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { Transform } from 'stream'
 
-await transform();
+const transform = async () => {
+	try {
+		const reverseTransform = new Transform({
+			transform(chunk, _, callback) {
+				const reversedChunk = chunk.toString().split('').reverse().join('')
+				this.push(reversedChunk)
+				callback()
+			},
+		})
+
+		process.stdin.pipe(reverseTransform)
+
+		reverseTransform.pipe(process.stdout)
+
+		console.log('Enter text to reverse (Ctrl+C to end):')
+	} catch (error) {
+		console.error('Error during transformation:', error.message)
+	}
+}
+
+await transform()
