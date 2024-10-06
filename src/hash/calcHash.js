5ba295966eb349ba8dd4a23da6bcb43a8f49d10b
PopovaOlha <fc.odess.10@gmail.com>
2024-10-06 08:41:35 +0300

    implement sha256 hash calculation using streams api

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from 'crypto'
+import { createReadStream } from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-await calculateHash();
+const calculateHash = async () => {
+	const fileName = 'fileToCalculateHashFor.txt'
+	const currentDir = path.dirname(fileURLToPath(import.meta.url))
+	const filePath = path.join(currentDir, 'files', fileName)
+
+	try {
+		const stream = createReadStream(filePath)
+		const hash = createHash('sha256')
+
+		stream.on('data', data => {
+			hash.update(data)
+		})
+
+		stream.on('end', () => {
+			const hashResult = hash.digest('hex')
+			console.log(`SHA256 hash for ${fileName}: ${hashResult}`)
+		})
+	} catch (error) {
+		console.error('Error reading the file:', error.message)
+	}
+}
+
+await calculateHash()
