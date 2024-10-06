2c7722638f28d77d06ddd18896d6df853d18cfcc
PopovaOlha <fc.odess.10@gmail.com>
2024-10-06 08:47:01 +0300

    feat: implement writing data from process.stdin to file using writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
-const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream } from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-await write();
+const write = async () => {
+	const fileName = 'fileToWrite.txt'
+	const currentDir = path.dirname(fileURLToPath(import.meta.url))
+	const filePath = path.join(currentDir, 'files', fileName)
+
+	const writableStream = createWriteStream(filePath, { flags: 'a' })
+
+	process.stdin.pipe(writableStream)
+
+	console.log(`Writing data from process.stdin to ${fileName}...`)
+}
+
+await write()
