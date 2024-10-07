5b1f63ca06879afa9d9557c03d8f4ddcf8069cc3
PopovaOlha <fc.odess.10@gmail.com>
2024-10-07 23:15:08 +0300

    feat: implement function that creates number of worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import os from 'os'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { Worker } from 'worker_threads'
 
-await performCalculations();
+const performCalculations = async () => {
+	const numCores = os.cpus().length
+	const results = []
+
+	const workers = Array.from({ length: numCores }, (_, index) => {
+		const currentDir = path.dirname(fileURLToPath(import.meta.url))
+		const workerPath = path.resolve(currentDir, 'worker.js')
+		const worker = new Worker(workerPath, { workerData: 10 + index })
+
+		worker.on('message', message => {
+			results.push(message)
+			if (results.length === numCores) {
+				console.log(results)
+			}
+		})
+
+		return worker
+	})
+
+	await Promise.all(
+		workers.map(worker => new Promise(resolve => worker.on('exit', resolve)))
+	)
+}
+
+await performCalculations()
