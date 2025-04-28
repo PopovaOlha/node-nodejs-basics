20ea426d09205722b791af6f9ec2a1c3e91989a4
Olha Popova <fc.odessa.10@gmail.com>
2025-04-28 13:43:15 -0700

    feat: implement spawnChildProcess with IPC between master and child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourceFile = 'files/script.js';
+const sourceFilePath = path.join(__dirname, sourceFile);
+
+const spawnChildProcess = async (filePath, args) => {
+  const argsToPass = args ?? [];
+
+  spawn('node', [filePath, ...argsToPass], {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(sourceFilePath, ['arg1', 'arg2']);
+
+
